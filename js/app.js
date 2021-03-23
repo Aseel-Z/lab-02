e00be976a5cccc8877c73cddd455630d0ee39d00
Aseel-Z <aseelalzweri@gmail.com>
2021-03-23 16:14:32 +0200

    lab-02-Final-Version

--- js/app.js
+++ js/app.js
@@ -2,27 +2,29 @@
 
 // constructor function for creating images objects, such that properties - from data json file, containns objects
 let keyArr = [];
-let selectedKey = "";
+let all = [];
 function Image(image) {
   this.image_url = image.image_url;
   this.title = image.title;
   this.description = image.description;
   this.keyword = image.keyword;
   this.horns = image.horns;
+  all.push(this)
   if (!keyArr.includes(this.keyword)) {
     keyArr.push(this.keyword);
   }
+
 }
 
 // declare the render function using protoype, jQuery css selectors/DOM
 
 Image.prototype.render = function () {
   $("main").append(`   
-    <template id="photo-template">
+    <div id="photo-template">
     <h2> ${this.title}</h2>
     <img src="${this.image_url}" alt="${this.title}">
     <p>${this.description}</p>
-  </template>`);
+  </div>`);
 };
 
 // declare the clone/copy function using protoype, , jQuery css selectors/DOM
@@ -33,8 +35,8 @@ Image.prototype.copyImageTemplate = function () {
   imageGen.find("p").text(this.description);
   imageGen.find("img").attr("src", this.image_url);
   imageGen.removeAttr("id");
-  imageGen.attr("id", this.title);
-  imageGen.attr("class", "imageRend");
+  imageGen.attr("id", `${this.keyword}`);
+  imageGen.attr("class",`${this.keyword}`);
   $("main").append(imageGen);
 };
 
@@ -54,7 +56,7 @@ const getData = {
 // pass on each object in the array of data, create new object, then save in a variable, then render that object, that image, clone, copy ,prototype such that the new object can access it
 // clone inside for loop, for each
 
-$.ajax("./data/page-1.json", getData).then((dataArray) => {
+$.ajax("data/page-1.json", getData).then((dataArray) => {
   console.log(dataArray);
   dataArray.forEach((element) => {
     // console.log(element)
@@ -62,23 +64,12 @@ $.ajax("./data/page-1.json", getData).then((dataArray) => {
     // console.log(imageNew)
     imageNew.copyImageTemplate();
   });
-  // render images with selected keyword - event
+  console.log(dataArray)
   keyArr.forEach((element) => {
     $("select").append(`<option value=${element}>${element}</option>`);
   });
   console.log(keyArr);
-  // render selected images
-  $("#select-list").on("click", function (event) {
-    Image.removeAll();
-    dataArray.forEach((element) => {
-      if (event.target.value === element.keyword) {
-        console.log(event.target.value === element.keyword);
-        console.log(element.keyword);
-        let imageNew = new Image(element);
-        imageNew.copyImageTemplate();
-      }
-    });
-  });
+
 });
 
 // render selected images
@@ -100,3 +91,40 @@ $.ajax("./data/page-1.json", getData).then((dataArray) => {
 //   });
 
 //   console.log(selectedKey)
+
+// $("#select-list").on("click", function (event) {
+//   Image.removeAll();
+//   allHorns.forEach((element) => {
+//     if (event.target.id === element.keyword)
+      
+//   });
+// });
+
+// $("select").change(function () {
+
+//   let selected= $(this).val()
+//  all.forEach(element => {
+//     if (selected === element.keyword) {
+//       $(`.${element.keyword}`).addClass("on")
+//     }
+//     else {
+//       $(`.${element.keyword}"`).addClass("off")
+//     }
+//   });
+  
+// })
+
+// console.log(all)
+
+
+$("select").change(function () {
+  $("main").html("")
+  let selected= $(this).val()
+ all.forEach(element => {
+   console.log(selected)
+   if (selected === element.keyword) {
+    element.copyImageTemplate()
+ }  
+ })
+})
+
